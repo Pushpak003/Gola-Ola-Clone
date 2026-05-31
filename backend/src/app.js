70f030b0c369a26b7df321c4c375571b60899d1f
Pushpak003 <pushpakpathe63@gmail.com>
2026-05-31 15:53:21 +0530

    fix: refine CORS configuration to specify allowed methods and credentials

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cors({origin: "*", methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],  credentials: false,}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/captain", captainRoutes);
